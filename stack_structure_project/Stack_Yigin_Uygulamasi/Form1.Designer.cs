7a8c655dbb95577e270cea3a18a42eb5d2702089
alperenalp <alperen.alp045@gmail.com>
2022-03-19 13:48:14 +0300

    project

---
+++ stack_structure_project/Stack_Yigin_Uygulamasi/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Stack_Yigin_Uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.cıkarButon = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ekleButon = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(586, 442);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.cıkarButon);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.ekleButon);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(578, 416);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Yığın Yapısı";
+            // 
+            // cıkarButon
+            // 
+            this.cıkarButon.Location = new System.Drawing.Point(372, 84);
+            this.cıkarButon.Name = "cıkarButon";
+            this.cıkarButon.Size = new System.Drawing.Size(82, 43);
+            this.cıkarButon.TabIndex = 3;
+            this.cıkarButon.Text = "ÇIKAR";
+            this.cıkarButon.UseVisualStyleBackColor = true;
+            this.cıkarButon.Click += new System.EventHandler(this.cıkarButon_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(237, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(129, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ekleButon
+            // 
+            this.ekleButon.Location = new System.Drawing.Point(149, 83);
+            this.ekleButon.Name = "ekleButon";
+            this.ekleButon.Size = new System.Drawing.Size(82, 43);
+            this.ekleButon.TabIndex = 0;
+            this.ekleButon.Text = "EKLE";
+            this.ekleButon.UseVisualStyleBackColor = true;
+            this.ekleButon.Click += new System.EventHandler(this.ekleButon_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(578, 416);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Yığının Boyutu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 442);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button cıkarButon;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ekleButon;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ stack_structure_project/Stack_Yigin_Uygulamasi/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stack_Yigin_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public class Dugum
+        {
+            public int sayi;
+            public Dugum baglanti;
+        }
+        Dugum ilk;
+
+        private void ekleButon_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!="")
+            {
+                Dugum yeni = new Dugum();
+                yeni.sayi = Convert.ToInt32(textBox1.Text);
+                yeni.baglanti = ilk;
+                ilk = yeni;
+                yiginGoster();
+            }
+            
+        }
+
+        private void yiginGoster()
+        {
+            int yiginBoyut = 0;
+            listBox1.Items.Clear();
+            if (ilk==null)
+            {
+                MessageBox.Show("Yığın Boşaltıldı.");
+                return;
+            }
+            else
+            {
+                Dugum isaretci = ilk;
+                while (isaretci != null)
+                {
+                    yiginBoyut++;
+                    listBox1.Items.Add(isaretci.sayi);
+                    isaretci = isaretci.baglanti;
+                }
+            }
+            label1.Text = "Yığının Boyutu: " + yiginBoyut.ToString();
+        }
+
+        private void cıkarButon_Click(object sender, EventArgs e)
+        {
+            if (ilk==null)
+            {
+                MessageBox.Show("Yığın Boş");
+                return;
+            }
+            ilk = ilk.baglanti;
+            yiginGoster();
+        }
+    }
+}
